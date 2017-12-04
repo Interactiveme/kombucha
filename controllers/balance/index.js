5561d5766dffd2545f05a339d7c9d3ac8cb43ec4
Mike <mike@ediy.co.nz>
2017-12-04 17:44:45 +1300

    Fixed private post

--- controllers/balance/index.js
+++ controllers/balance/index.js
@@ -10,30 +10,31 @@ module.exports = function (router) {
     router.get('/', function (req, res) {
 
         Settings.findOne({
-            user_id : req.user._id
-        }, function(err, settings) {
+            user_id: req.user._id
+        }, function (err, settings) {
             //If something weird happens, abort.
             if (err) {
                 console.log(err);
                 
             }
-
-            if (settings) {
-                var api = new Cryptopia(settings.api_key, settings.api_secret);
-                var balanceObject = api.getBalance(function(error, data){
+            try {
+                if (settings) {
+                    var api = new Cryptopia(settings.api_key, settings.api_secret);
+                    var balanceObject = api.getBalance(function (error, data) {
                         console.log(data);
-                },"btc");
+                    }, "BTC");
 
-                console.log("balanceObject",balanceObject);
-                model.balanceObject = balanceObject;
-                res.render('balance', model);
+                    console.log("balanceObject", balanceObject);
+                    model.balanceObject = balanceObject;
+                    res.render('balance', model);
                 
-            } else {
-                console.log("no settings found");
+                } else {
+                    console.log("no settings found");
                 
+                }
             }
+            catch (ex) { }
         });
-
      });
      
 };
